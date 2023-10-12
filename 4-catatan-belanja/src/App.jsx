daee2a91e72f4d1351c270baf362969ed3e67809
Rully Gandadiwirya <rullyrgandadiwirya@gmail.com>
2023-10-12 21:51:14 +0700

    Menambahkan item

--- 4-catatan-belanja/src/App.jsx
+++ 4-catatan-belanja/src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const groceryItems = [
   {
     id: 1,
@@ -38,19 +40,36 @@ function Header(){
 
 function Form(){
 
-  const quantityNum = [...Array(20)].map((_, i) => (
-    <option value={i + 1} key = {i + 1}>{i + 1}</option>
+  const [name, setName] = useState('');
+
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e){
+    e.preventDefault();
+    
+    if(!name) return ;
+    
+    const newItem = {name, quantity, checked: false, id: Date.now() };
+
+    setName('');
+    setQuantity(1);
+  }
+
+  const quantityNum = [...Array(10)].map((_, i) => (
+    <option value={i + 1} key = {i + 1}>
+      {i + 1}
+    </option>
   ));
 
 
   return (
-    <form className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>Hari ini belanja apa kita?</h3>
       <div>
-        <select>
+        <select value={quantity} onChange={(e) => setQuantity(Number(e.target.value))}>
           {quantityNum}
         </select>
-        <input type="text" placeholder="nama barang..." />
+        <input type="text" placeholder="nama barang..." value={name} onChange={(e) => setName(e.target.value)} />
       </div>
       <button>Tambah</button>
       </form>
